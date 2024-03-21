2787bf0cc082a7819f0b989ca5cc5785d4ca5ab7
Aadhavan <aadhavanlenin@gmail.com>
2024-03-22 02:07:57 +0530

    base url

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
 	title: 'eXpServer',
 	description: 'Documentation for eXpServer',
+	base: '/',
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		logo: '/assets/icon.png',
